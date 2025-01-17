d36e9d7e4c8f007d87a9cd81e31b8faaae273fdb
ken13-spect <00120620@uca.edu.sv>
2025-01-17 17:05:55 -0600

    :safety_vest: feat(auth): Add validations to DTOs for register and login

--- src/auth/auth.guard/auth.guard.ts
+++ src/auth/auth.guard/auth.guard.ts
@@ -10,35 +10,29 @@ import { JwtService } from '@nestjs/jwt';
 import { jwtConstants } from '../constants';
 import { Request } from 'express';
 
-
 @Injectable()
 export class AuthGuard implements CanActivate {
- 
   constructor(private jwtService: JwtService) {}
 
-  async canActivate(
-    context: ExecutionContext
-  ):  Promise<boolean>{
-      //get the request
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    //get the request
     const request = context.switchToHttp().getRequest();
     //get the token from the request headers
     const access_token = this.extractTokenFromHeader(request);
     //check if token exists
-    if (!access_token) 
+    if (!access_token)
       throw new HttpException('Token not found', HttpStatus.UNAUTHORIZED);
-
     //verify the token
-    try{
-      const payload = await this.jwtService.verifyAsync(
-        access_token,
-        { secret: jwtConstants.secret}
-      )
+    try {
+      const payload = await this.jwtService.verifyAsync(access_token, {
+        secret: jwtConstants.secret,
+      });
       //set payload to the request object
       request.user = payload;
-    }catch(e){
+    } catch (e) {
       return false;
     }
-    
+
     return true;
   }
 
@@ -47,4 +41,3 @@ export class AuthGuard implements CanActivate {
     return type === 'Bearer' ? token : undefined;
   }
 }
-
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, Matches, Max, MaxLength, Min, MinLength } from "class-validator";
+
 export class CreateUserDto {
-    name: string;
+    @IsString()
+    @MaxLength(20)
+    @MinLength(3)
+    name?: string;
+
+    @IsEmail()
+    @IsString()
+    @IsNotEmpty()
     email: string;
+ 
+    @IsNotEmpty()
+    @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
+        message: 'The password must have at least 1 number, 1 letter, 1 special character, and a length of 8 ',})
     password: string;
 }
