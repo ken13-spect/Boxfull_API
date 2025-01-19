ca39197b28b50fbb68bf5ae36bee804159b924d3
ken13-spect <00120620@uca.edu.sv>
2025-01-19 01:43:34 -0600

    :wrench: config(cords): Enable CORS for all origins

--- src/users/dto/login-user-dto.ts
+++ src/users/dto/login-user-dto.ts
@@ -4,7 +4,6 @@ import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 export class LoginUserDTO {
     @IsEmail()
     @IsNotEmpty()
-    @IsString()
     email: string;
 
     @IsNotEmpty()
